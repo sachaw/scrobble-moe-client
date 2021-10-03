3bff295eca71bf2a5a87fd70138761575172c897
Sacha Weatherstone <sachaw100@hotmail.com>
2021-10-03 23:37:30 +1100

    Fix Build

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,17 +4,15 @@ import { FiClipboard } from 'react-icons/fi';
 
 type DefaulInputProps = JSX.IntrinsicElements["input"];
 
-export interface InputProps extends DefaulInputProps {}
-
-export const Input = ({ ...props }: InputProps): JSX.Element => {
+export const Input = ({ ...props }: DefaulInputProps): JSX.Element => {
   return (
-    <div className="w-full flex rounded-md border border-gray-200 hover:border-gray-400 ">
+    <div className="flex w-full border border-gray-200 rounded-md hover:border-gray-400 ">
       <input
-        className="h-10 w-full bg-transparent px-3 py-2 focus:outline-none focus:border-pink-400"
+        className="w-full h-10 px-3 py-2 bg-transparent focus:outline-none focus:border-pink-400"
         {...props}
       />
-      <div className="flex hover:bg-gray-200 rounded-r-md cursor-pointer text-gray-600 hover:text-foreground">
-        <div className="my-auto px-2 ">
+      <div className="flex text-gray-600 cursor-pointer hover:bg-gray-200 rounded-r-md hover:text-foreground">
+        <div className="px-2 my-auto ">
           <FiClipboard />
         </div>
       </div>
