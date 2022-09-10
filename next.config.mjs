437cdb62271337d526844cb1995c7579983f78e7
Sacha Weatherstone <sachaw100@hotmail.com>
2022-09-10 21:18:27 +1000

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import withTM from "next-transpile-modules";
+
+const transpile = withTM(["@simplewebauthn/browser"]);
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -5,37 +9,12 @@ const nextConfig = {
   images: {
     domains: ["plex.tv", "devicons.railway.app", "s4.anilist.co"],
   },
+  swcMinify: true,
   reactStrictMode: true,
   experimental: {
     concurrentFeatures: true,
     serverComponents: true,
-    // esmExternals
   },
 };
 
-// const withPWA = require("next-pwa");
-// const withTM = require("next-transpile-modules")(["@simplewebauthn/browser"]);
-
-//  const nextConfig = withPWA(
-//   withTM({
-//     pwa: {
-//       disable:
-//         process.env.NODE_ENV === "development" ||
-//         process.env.NODE_ENV === "preview" ||
-//         process.env.NODE_ENV === "production",
-//       // delete two lines above to enable PWA in production deployment
-//       // add your own icons to public/manifest.json
-//       // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-//       dest: "public",
-//       register: true,
-//     },
-//     images: {
-//       domains: ["plex.tv", "devicons.railway.app", "s4.anilist.co"],
-//     },
-//     experimental: { esmExternals: true },
-//     esmExternals: true,
-//     reactStrictMode: true,
-//   })
-// );
-
-export default nextConfig;
+export default transpile(nextConfig);
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,27 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import { NavButton } from 'components/NavButton';
-import {
-  FiActivity,
-  FiDatabase,
-  FiKey,
-  FiLayout,
-  FiRss,
-  FiVideo,
-} from 'react-icons/fi';
+import { NavButton } from "components/NavButton";
+import { FiActivity, FiKey, FiLayout } from "react-icons/fi";
 
 export const Sidebar = (): JSX.Element => {
   return (
     <aside className="w-52 min-w-[13rem] pt-4 top-4 sticky space-y-2">
       <NavButton href="/dashboard" icon={<FiActivity />} text="Dashboard" />
       <NavButton href="/credentials" icon={<FiKey />} text="Credentials" />
-      <NavButton
-        href="/torrentClient"
-        icon={<FiDatabase />}
-        text="Torrent Client"
-      />
-      <NavButton href="/encoders" icon={<FiVideo />} text="Encoders" />
-      <NavButton href="/subscriptions" icon={<FiRss />} text="Subscriptions" />
       <NavButton href="/admin" icon={<FiLayout />} text="Admin" />
     </aside>
   );
